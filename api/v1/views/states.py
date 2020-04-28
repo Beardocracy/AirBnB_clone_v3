987b35086e10f2516ca1338fe431b379133debfa
Beardocracy <travisjbearden@gmail.com>
2020-04-28 17:29:07 +0000

    Added states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 
 from models.state import State
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from models import storage
 
 
@@ -14,3 +14,12 @@ def list_all_states():
     for state in storage.all('State').values():
         all_states.append(state.to_dict())
     return jsonify(all_states)
+
+
+@app_views.route('/api/v1/states/<state_id>', strict_slashes=False)
+def list_state_by_id(state_id):
+    """ List a state if the id exists """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
