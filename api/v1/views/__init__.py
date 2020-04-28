6e0f80b7c3b8dd25c3a48629c3c9cbf2447e616e
ariana124 <ariana.bibiano@gmail.com>
2020-04-28 19:32:47 +0000

    fixed pep8 issues

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Initialize Flask and Blueprint """
-
-
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__)
+"""
+Initializes Flask and Blueprint
+"""
 from api.v1.views.index import *
 from api.v1.views.states import *
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__)
