313cef8b843d5056ccffd72492ab3d290cd19334
ariana124 <ariana.bibiano@gmail.com>
2020-04-28 18:57:45 +0000

    added count object stats method and imported jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-""" Blueprint description for index """
-
+"""
+Blueprint description for index
+"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('/api/v1/status', strict_slashes=False)
 def status():
     """ Returns status message """
-    return {"status": "OK"}
+    status = {"status": "OK"}
+    return jsonify(status)
+
+
+@app_views.route('stats', strict_slashes=False)
+def obj_stats():
+    """ Retrieves the number of each object by type """
+    classes = {"amenities": "Amenity",
+               "cities": "City",
+               "places": "Place",
+               "states": "State",
+               "users": "User"}
+    for key, value in classes.items():
+        classes[key] = storage.count(value)
+    return jsonify(classes)
