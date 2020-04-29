76844b8f225d47280f29129701d649ebdc3128a7
ariana124 <ariana.bibiano@gmail.com>
2020-04-29 00:45:51 +0000

    added /api/v1/ in front of paths

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,7 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<states_id>/cities', methods=['GET'],
+@app_views.route('/api/v1/states/<states_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def list_cities():
     """ lists all cities in JSON format """
@@ -23,7 +23,7 @@ def list_cities():
     return jsonify(city_list), 200
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'],
+@app_views.route('/api/v1/cities/<city_id>', methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id):
     """ retrieves a city """
@@ -33,7 +33,7 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'],
+@app_views.route('/api/v1/cities/<city_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_city(city_id):
     """ deletes a city object """
@@ -45,7 +45,7 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
+@app_views.route('/api/v1/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def create_city(state_id):
     """ creates a city object """
@@ -64,7 +64,7 @@ def create_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'],
+@app_views.route('/api/v1/cities/<city_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_city(city_id):
     """ updates a city object """
